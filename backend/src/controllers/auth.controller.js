6a6e9afcd2ccb84b263ca799d015e96a5e0161d8
erdaserhan <erhan209@gmail.com>
2025-03-13 17:35:37 +0100

    Mongoose DB connection ok

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const singup = async (req, res) => {
+    res.send("singup route");
+};
+
+export const login = async (req, res) => {
+    res.send("login route");
+};
+
+export const logout = async (req, res) => {
+    res.send("logout route");
+};
