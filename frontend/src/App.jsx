ce727625b2352d6a34c37660f52311a87e655e1f
erdaserhan <erhan209@gmail.com>
2025-03-17 22:24:06 +0100

    Routes created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
+import SignUpPage from "./pages/SignupPage"
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+
+    </div>
   )
 }
 
