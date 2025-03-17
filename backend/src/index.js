a4d946a19b215e986639a63ef78ab5a8e17ccae5
erdaserhan <erhan209@gmail.com>
2025-03-17 20:30:32 +0100

    getUsersForSidebar function created

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import CookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 
 import { connectDB } from './lib/db.js';
 
@@ -15,6 +16,7 @@ app.use(express.json()); // allow us to parse incoming JSON data
 app.use(CookieParser()); // allow us to parse incoming cookie data
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on port:', PORT);
