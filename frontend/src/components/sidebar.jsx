92c4d513cf24444c3444770de9ab674a7478e2ec
erdaserhan <erhan209@gmail.com>
2025-03-20 13:32:33 +0100

    SidebarSkeleton created and sidebar updated

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore"
 
 const sidebar = () => {
+  const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
+
+  const onlineUsers = [];
+
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  if (isUsersLoading) return <SidebarSkeleton />;
+
   return (
     <div>sidebar</div>
   )
