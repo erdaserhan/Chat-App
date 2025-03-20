eac33da3502c370101a34d2ffdcb8653478b0d75
erdaserhan <erhan209@gmail.com>
2025-03-20 13:23:32 +0100

    NoChatSelected component ok

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
-import { getMessages } from "../../../backend/src/controllers/message.controller";
 
 export const useChatStore = create((set) => ({
     messages: [],
