2e805c0f49e5ea79a95e3de5fd0b99c3c276f6f4
erdaserhan <erhan209@gmail.com>
2025-03-26 15:21:56 +0100

    MODE s updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import CookieParser from 'cookie-parser';
 import cors from 'cors';
 
+import path from 'path';
+
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
@@ -12,6 +14,7 @@ import { app, server } from './lib/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "50mb" })); // allow us to parse incoming JSON data
 app.use(CookieParser()); // allow us to parse incoming cookie data
@@ -24,6 +27,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log('Server is running on port:', PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    // if we are in development mode, we will use the development server
+    // if we are in production mode, we will use the production
+    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : '/api',
     withCredentials: true, // sending cookies automatically with each request
 });
